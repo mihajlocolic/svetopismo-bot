3182e6d556c7bee335e2d24c09f5163c981014c8
Mihajlo Colic <colicmihajlo@gmail.com>
2025-09-08 14:02:45 +0200

    Initial commit

---
+++ bot.js
@@ -0,0 +1,13 @@
+const {Client, Events, GatewayIntentBits} = require('discord.js');
+const dotenv = require('dotenv').config(); 
+
+const botToken = process.env.BOT_TOKEN;
+
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`[Ready] Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(botToken);
+
