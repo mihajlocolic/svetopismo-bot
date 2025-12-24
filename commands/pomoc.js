e560b9f2bf9cbb0e4469a1c598920d5cccbab3cb
Mihajlo Colic <colicmihajlo@gmail.com>
2025-12-24 14:36:01 +0100

    Added simple help command

---
+++ commands/pomoc.js
@@ -0,0 +1,21 @@
+const {SlashCommandBuilder, EmbedBuilder, Colors, MessageFlags} = require('discord.js');
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("помоћ")
+        .setDescription("Приказ помоћи за коришћење бота."),
+    async execute(interaction) {
+        await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+        const embed = new EmbedBuilder()
+            .setTitle("Помоћ за коришћење бота")
+            .setColor(Colors.Red)
+            .setDescription("Овде можете пронаћи информације о доступним командама и њиховој употреби.")
+            .addFields(
+                { name: "/књиге", value: "Приказује списак свих доступних књига у Светоме Писму и њихове скраћенице." },
+                { name: "/претрага <књига (скраћеница)> <глава> <почетни-стих> <завршни-стих (опционо)>", value: "Претражује и приказује стихове из Светог Писма на основу унетих параметара.\n"}
+            )
+
+        await interaction.editReply({embeds: [embed]});
+    }
+}
